7eb2e53f484bdead827b2c567c6aeee1de0c63e9
hiiidee12 <125862664+hiiidee12@users.noreply.github.com>
2026-01-04 11:49:13 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,28 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { WagmiProvider } from 'wagmi'
+import { wagmiConfig } from '@/src/wagmi'
 
+export const metadata: Metadata = {
+  title: 'Farcasend',
+  description: 'Batch transfer ETH/tokens to many wallets from Farcaster',
+  openGraph: {
+    title: 'Farcasend',
+    description: 'Batch transfer to many wallets',
+    url: 'https://farca-send.vercel.app',
+    images: ['https://farca-send.vercel.app/farca.png'],
+  },
+  icons: {
+    icon: '/farca.png',
+  },
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="id">
+      <body>
+        <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
+      </body>
+    </html>
+  )
+}
